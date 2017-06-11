5bef0cc3bc8533c12c7693e93a2397cef119fada
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-06-11 15:18:16 +0700

    text-to-speech and speech-to-text is working

--- android/app/src/main/java/com/reactnativefirst/MainApplication.java
+++ android/app/src/main/java/com/reactnativefirst/MainApplication.java
@@ -3,11 +3,13 @@ package com.reactnativefirst;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import co.ulimit.speechnotification.RCTSpeechNotification;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.wmjmc.reactspeech.VoicePackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RCTCameraPackage()
+            new RCTSpeechNotification(),
+            new RCTCameraPackage(),
+            new VoicePackage()            
       );
     }
   };
